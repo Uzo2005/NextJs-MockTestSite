44c0da77d758ca9ad9c5ed315e3ce6d17e84a50b
Uzo2005 <igwecletus455@gmail.com>
2022-08-19 14:09:21 +0100

    changed

--- pages/api/initExam.js
+++ pages/api/initExam.js
@@ -1,13 +1,14 @@
 import { withSessionRoute } from "../../lib/withSessions";
 
-export default withSessionRoute(async function initExam(req, res, session) {
+export default withSessionRoute(async function initExam(req, res) {
   try{
       if (req.method == "POST") {
         req.session.examInfo = {
           id: req.body.examId,
         };
         await req.session.save();
-        res.redirect("/firstInstructions");
+        res.redirect(302, "/firstInstructions");
+        res.end()
   }
   } catch (err) {
     console.log(err)
