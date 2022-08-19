caeb836e7fed6daf107463da34f9c0cb51adf6ef
Uzo2005 <igwecletus455@gmail.com>
2022-08-19 13:28:34 +0100

    please workgit add .!

--- pages/api/initExam.js
+++ pages/api/initExam.js
@@ -1,12 +1,16 @@
 import { withSessionRoute } from "../../lib/withSessions";
 
 export default withSessionRoute(async function initExam(req, res, session) {
-  if (req.method == "POST") {
-    req.session.examInfo = {
-      id: req.body.examId,
-    };
-    await req.session.save();
-    console.log('dashboard',req.session.examInfo)
-    res.redirect("/firstInstructions");
+  try{
+      if (req.method == "POST") {
+        req.session.examInfo = {
+          id: req.body.examId,
+        };
+        await req.session.save();
+        res.redirect("/firstInstructions");
   }
+  } catch (err) {
+    console.log(err)
+  }
+  
 });
